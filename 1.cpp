500d5eb4a7d8bbe3a86b1ddc8619a249b250c10f
dead1971de <91102950+dead1971de@users.noreply.github.com>
2021-10-03 19:31:57 +0300

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <math.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int x1, y1, x2, y2, a, b;
+
+    cout << "Даны 2 точки: A(x1, x2) ; B(y1, y2)" << endl;
+    cout << endl;
+    cout << "Введите их координаты, чтобы узнать расстояние между этими точками на плоскости: ";
+    cin >> x1 >> x2 >> y1 >> y2;
+    cout << endl;
+
+    double AB = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    cout << "Расстояние между точками A(" << x1 << "," << x2 << ")" << " и B(" << y1 << "," << y2 << "): " << AB << endl;
+        
+
+
+}
+
---
+++ 5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double x1, y1, x2, y2, x3, y3, P, S, p, A, B, C;
+
+	cout << "Введите координаты 3 вершин треугольника." << endl << "x1 , y1 : ";
+	cin >> x1 >> y1;
+	cout << "x2 , y2 : ";
+	cin >> x2 >> y2;
+	cout << "x3 , y3 : ";
+	cin >> x3 >> y3;
+	cout << endl;
+
+	A = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
+	B = sqrt(pow(x2 - x3, 2) + pow(y2 - y3, 2));
+	C = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
+	P = A + B + C;
+
+	p = P / 2;
+
+	S = sqrt(p * (p - A) * (p - B) * (p - C));
+
+	cout << "Периметр треугольника : " << P << endl << "Площадь прямоугольника : " << S << endl;
+
+}      
