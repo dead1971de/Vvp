384e6c9ebfd47e92958ae3caff3caedfed92801e
dead1971de <91102950+dead1971de@users.noreply.github.com>
2021-10-05 01:36:40 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int a1, b1, a2, b2, P, S;
+    
+    cout << "Введите значения координат a1, b1 , a2 , b2" << endl;
+    cin >> a1 >> b1 >> a2 >> b2;
+    cout << endl;
+
+    P = 2 * abs((a1 - a2) + (b1 - b2));
+    S = abs((a1 - a2) * (b1 - b2));
+
+    cout << "Периметр прямоугольника : " << P;
+    cout << "Площадь прямоугольника : " << S;
+}
+
+
