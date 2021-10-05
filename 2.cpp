2e2d2f1f879db39c29cc97ab67bd727f73af05f2
dead1971de <91102950+dead1971de@users.noreply.github.com>
2021-10-05 16:47:53 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+
+    float A, B, C, AC, BC;
+
+    cout << "Введите A: ";
+    cin >> A;
+    cout << endl;
+
+    cout << "Введите B: ";
+    cin >> B;
+    cout << endl;
+
+    cout << "Введите C: ";
+    cin >> C;
+    cout << endl;
+
+
+    AC = abs(C - A);
+    BC = abs(C - B);
+
+    cout << "Длина отрезка AC: " << AC << endl;
+    cout << "Длина отрезка BC: " << BC << endl;
+    cout << "Сумма отрезков AC и BC: " << AC + BC << endl;
+}
---
+++ 3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	int A, B, C, AC, BC;
+
+	cout << "Введите значение точек A , B , C : ";
+	cin >> A >> B >> C;
+	cout << endl;
+
+	AC = abs(C - A);
+	BC = abs(C - B);
+
+	if (C < A , C > B)
+	{
+		cout << "Некорректное значение С.";
+	}
+	else
+	{
+		AC = abs(C - A);
+		BC = abs(C - B);
+		cout << "Произведение отрезков AC и BC : " << AC * BC << endl;
+	}
+
+	
+}
