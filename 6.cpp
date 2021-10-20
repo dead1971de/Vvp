41098e4f122f312cd67f17091e834cdcb2f85271
dead1971de <91102950+dead1971de@users.noreply.github.com>
2021-10-20 20:51:08 +0400

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,22 +1,31 @@
-﻿#include <iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-    
-    setlocale(LC_ALL, "RUS");
 
-    float A1, A2, B1, B2, C1, C2, x, y;
+	setlocale(LC_ALL, "RUS");
 
-    cout << "Введите A1, B1, C1, A2 , B2, C2 : ";
-    cin >> A1 >> B1 >> C1 >> A2 >> B2 >> C2;
-    cout << endl;
+	float A1, A2, B1, B2, C1, C2, x, y, D1, D2, D3;
 
-    y = (A1 * C2 - A2 * C1) / (A1 * B2 - A2 * B1);
+	cout << "Введите A1, B1, C1, A2 , B2, C2 : ";
+	cin >> A1 >> B1 >> C1 >> A2 >> B2 >> C2;
+	cout << endl;
 
-    x = (C1 - B1 * y) / A1;
 
-        cout << "X = " << x << endl << "Y = " << y << endl;
+	D2 = A1 * C2 - C1 * A2;
 
+	D1 = A1 * B2 - A2 * B1;
+
+	x = D2 / D1;
+
+	D3 = C1 * B2 - B1 * C2;
+
+	y = D3 / D1;
+
+	if (y == -0) { y = 0; }
+	if (x == -0) { x = 0; }
+
+	cout << "X = " << x << endl << "Y = " << y << endl;
 
 }
