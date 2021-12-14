e10bd54f360b4d38021bdbf4bcc20610e6c611c7
dead1971de <91102950+dead1971de@users.noreply.github.com>
2021-12-14 18:14:00 +0400

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -6,7 +6,7 @@ int main()
     setlocale(LC_ALL, "RUS");
 
     int N, K = 0, A = 0;
-    cout << "Введите значение переменной K (K > 1) : ";
+    cout << "Введите значение переменной N (N > 1) : ";
     cin >> N;
 
     while (A <= N)
